d12fe97910cf5221c9b9ce9bdda9293a75a22a31
j-degroot <jurrengroot@hotmail.nl>
2022-04-16 15:14:09 +0200

    comments for main.py

--- main.py
+++ main.py
@@ -26,55 +26,57 @@ def main():
             print(graphs)
 
     except:
+
+        ## read SMILES
         print('Could not load data. \n Processing data.')
 
         df = pd.read_csv('DrugEx/data/LIGAND_RAW.tsv', sep='\t', header=0)
-
-        print(df.columns, '\n', df.shape)
-
-
         df = df[['Smiles', 'pChEMBL_Value']]
         df = df.dropna(axis=0)  # drop rows with missing values
-
         # smile = smile.replace('[O]', 'O').replace('[C]', 'C') \
         #     .replace('[N]', 'N').replace('[B]', 'B') \
         #     .replace('[2H]', '[H]').replace('[3H]', '[H]')
-
         PandasTools.AddMoleculeColumnToFrame(df,'Smiles','Molecule',includeFingerprints=False)
-
         df['Smiles'] = df['Smiles'].map(canonical_smiles)
-
         df = df.drop_duplicates(subset=['Smiles'])
 
-        print(df.shape)
 
+        # REGRESSION TARGET VALUES
         target_values = df['pChEMBL_Value'].to_numpy()
 
-        fps = calc_fps(df['Molecule'])
 
-        graphs = np.array([[]])
+        # FEATURIZATION OF MOLECULES
+        fps = calc_fps(df['Molecule'])  # FINGERPRINT METHOD (DrugEX method)
+
+        graphs = np.array([[]])  # TUTORIAL METHOD (JOHN BRADSHAW)
         for mol in df['Molecule']:
             graphs = np.append(graphs, Graphs.from_mol(mol))
 
+        # SAVING FEAUTRIZED MOLECULES
         with open('data/ligand_processed.npy', 'wb') as f:
             np.save(f, target_values)
             np.save(f, fps)
             np.save(f, graphs)
 
 
+    # MAKING TRAIN AND TEST SET (validation?)
     y_train, y_test, fps_train, fps_test, graphs_train, graphs_test = train_test_split(target_values, fps, graphs, test_size=0.2)
-    # xgb = XGBRegressor()
-    # xgb.fit(train_X_fp, train_y)
-    #
-    # test_y_pred = xgb.predict(test_X_fp)
-    # print(mean_squared_error(test_y, test_y_pred))
 
 
+    # THIS USES DRUGEX METHOD - FOR BENCHMARKING
+    xgb = XGBRegressor()
+    xgb.fit(fps_train, y_train)
 
+    y_test_pred = xgb.predict(fps_test)
+    print(mean_squared_error(y_test, y_test_pred))
+
+
+    # GNN METHOD
     gnn = GNN(len(Graphs.ATOM_FEATURIZER.indx2atm))
 
-
-
+    # # train_neural_network in network.py script!
+    # # we need graphs to be a torch.data.Dataloader
+    ### out = train_neural_network(train_dataset=graphs_train_dataloader, val_dataset=graphs_val_dataloader #...ETC)
 
     plot_train_and_val_using_altair(out['train_loss_list'], out['val_lost_list'])
     # print(graphs.shape)
