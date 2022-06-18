e6e59cb061f4e1a343d8089e55644b03bf7fa01b
j-degroot <jurrengroot@hotmail.nl>
2022-06-18 12:40:41 +0200

    DataModule protein categorical encoding and PyG Data.y saving
    - Save PyG Data.y with shape (1,1), so we do not require reshaping in the MSE loss

--- data_module.py
+++ data_module.py
@@ -10,7 +10,7 @@ import pytorch_lightning as pl
 from pytorch_lightning.utilities.types import TRAIN_DATALOADERS, EVAL_DATALOADERS
 
 from utils.from_smiles import from_smiles
-from utils.encode_protein import prot_target2array
+from utils.encode_protein import prot_target2array, prot_target2one_hot
 
 from sklearn.model_selection import train_test_split
 
@@ -18,17 +18,18 @@ print(f"Torch version: {torch.__version__}")
 print(f"Cuda available: {torch.cuda.is_available()}")
 print(f"Torch geometric version: {torch_geometric.__version__}")
 
+
 # https://github.com/deepfindr/gnn-project/blob/main/dataset_featurizer.py
 class MoleculeDataset(Dataset):
-    def __init__(self, root, filename, test=False, transform=None, pre_transform=None):
+    def __init__(self, root, filename, prot_target_encoding=None, test=False, transform=None, pre_transform=None):
         """
         root = Where the dataset should be stored. This folder is split
         into raw_dir (downloaded dataset) and processed_dir (processed data).
         """
         self.test = test
         self.filename = filename
+        self.prot_target_encoding = prot_target_encoding
         super(MoleculeDataset, self).__init__(root, transform, pre_transform)
-
 
     @property
     def raw_file_names(self):
@@ -52,11 +53,16 @@ class MoleculeDataset(Dataset):
 
     def process(self):
         self.data = pd.read_csv(self.raw_paths[0]).reset_index()
-        # self.data = self.data.head(1000)
+        if self.prot_target_encoding != None:
+            prot_target_encoder = self._target_encoder(self.prot_target_encoding)
+        # self.data = self.data.head(400)  # for debugging purposes
         for index, mol in tqdm(self.data.iterrows(), total=self.data.shape[0]):
             # Featurize molecule into PyG graph object Data
             data = from_smiles(mol['SMILES'])
-            data.y = mol['pchembl_value_Mean']
+            # wrapping the value in a list to have the same shape as the network prediction
+            data.y = torch.tensor([[mol['pchembl_value_Mean']]])
+            if self.prot_target_encoding != None:
+                data.p = prot_target_encoder(mol['target_id'])
             if self.test:
                 torch.save(data,
                            os.path.join(self.processed_dir,
@@ -65,21 +71,26 @@ class MoleculeDataset(Dataset):
                 torch.save(data,
                            os.path.join(self.processed_dir,
                                         f'data_{index}.pt'))
-# When implementing two-to-one: 
-#   Decide how to save encoded protein targets, 
-#   this could be added to the above loop
-#         protein_targets = []
-#         encoded = dict()
-#         count = 0
-#         for target in mol['target_id']:
-#             if target in encoded.keys():
-#                 array = encoded[target]
-#             else:
-#                 array = prot_target2array(target)
-#                 encoded[target] = array
-#             protein_targets.append(array)
-#         print('Finished encoding protein targets')
-#         protein_targets = np.array(protein_targets)
+
+    def _target_encoder(self, target_encoding):
+        if target_encoding == 'one-hot-encoding':
+            return prot_target2one_hot
+
+    # When implementing two-to-one:
+    #   Decide how to save encoded protein targets,
+    #   this could be added to the above loop
+    #         protein_targets = []
+    #         encoded = dict()
+    #         count = 0
+    #         for target in mol['target_id']:
+    #             if target in encoded.keys():
+    #                 array = encoded[target]
+    #             else:
+    #                 array = prot_target2array(target)
+    #                 encoded[target] = array
+    #             protein_targets.append(array)
+    #         print('Finished encoding protein targets')
+    #         protein_targets = np.array(protein_targets)
 
     def len(self):
         return self.data.shape[0]
@@ -105,7 +116,7 @@ class GNNDataModule(pl.LightningDataModule):
         self.prepare_data_per_node = True
         self.batch_size = config['batch_size']
         self.num_workers = config['num_workers']
-        data_train, data_val = train_test_split(data_train, test_size = 0.1, random_state=0) #TODO add a randomstate
+        data_train, data_val = train_test_split(data_train, test_size=0.1, random_state=0)  # TODO add a randomstate
         self.train_data = data_train
         self.val_data = data_val
         self.test_data = data_test
@@ -126,7 +137,7 @@ class GNNDataModule(pl.LightningDataModule):
 
     def test_dataloader(self) -> EVAL_DATALOADERS:
         test_dataloader = DataLoader(dataset=self.test_data,
-                                    batch_size=self.batch_size,
-                                    shuffle=False,
-                                    num_workers=self.num_workers)
+                                     batch_size=self.batch_size,
+                                     shuffle=False,
+                                     num_workers=self.num_workers)
         return test_dataloader
