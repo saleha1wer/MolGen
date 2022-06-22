e849c649a1ad12b0d29e4a2ae02cb9aa7ff48021
j-degroot <jurrengroot@hotmail.nl>
2022-06-22 16:43:58 +0200

    Update encode_ligand.py

--- utils/encode_ligand.py
+++ utils/encode_ligand.py
@@ -14,7 +14,7 @@ from rdkit.Chem.GraphDescriptors import BertzCT
 
 
 # ADAPTED FROM PYTORCH-GEOMETRIC.UTILS.FROM_SMILES ---------------------------------------------------------------------
-# ADDED NUM_NODES AND NUM_FEATURES ARGUMENT
+# SAVING TENSORS AS FLOATS
 x_map = {
     'atomic_num':
         list(range(0, 119)),
@@ -66,14 +66,12 @@ e_map = {
 }
 
 
-def from_smiles(smiles: str, num_node_features: int, num_edge_features: int, with_hydrogen: bool = False,
+def from_smiles(smiles: str, with_hydrogen: bool = False,
                 kekulize: bool = False):
     r"""Converts a SMILES string to a :class:`torch_geometric.data.Data`
     instance.
 
     Args:
-        num_node_features:
-        num_edge_features:
         smiles (string, optional): The SMILES string.
         with_hydrogen (bool, optional): If set to :obj:`True`, will store
             hydrogens in the molecule graph. (default: :obj:`False`)
@@ -99,19 +97,18 @@ def from_smiles(smiles: str, num_node_features: int, num_edge_features: int, wit
     for atom in mol.GetAtoms():
         x = []
         x.append(x_map['atomic_num'].index(atom.GetAtomicNum()))
-        x.append(x_map['atomic_num'].index(atom.GetAtomicNum()))
         x.append(x_map['chirality'].index(str(atom.GetChiralTag())))
         x.append(x_map['degree'].index(atom.GetTotalDegree()))
         x.append(x_map['formal_charge'].index(atom.GetFormalCharge()))
         x.append(x_map['num_hs'].index(atom.GetTotalNumHs()))
-        x.append(x_map['num_radical_electrons'].index(atom.GetNumRadicalElectrons()))
+        x.append(x_map['num_radical_electrons'].index(
+            atom.GetNumRadicalElectrons()))
         x.append(x_map['hybridization'].index(str(atom.GetHybridization())))
         x.append(x_map['is_aromatic'].index(atom.GetIsAromatic()))
         x.append(x_map['is_in_ring'].index(atom.IsInRing()))
-        x = x[:num_node_features]
         xs.append(x)
 
-    x = torch.tensor(xs, dtype=torch.float).view(-1, num_node_features)
+    x = torch.tensor(xs, dtype=torch.float).view(-1, 9)
 
     edge_indices, edge_attrs = [], []
     for bond in mol.GetBonds():
@@ -122,14 +119,13 @@ def from_smiles(smiles: str, num_node_features: int, num_edge_features: int, wit
         e.append(e_map['bond_type'].index(str(bond.GetBondType())))
         e.append(e_map['stereo'].index(str(bond.GetStereo())))
         e.append(e_map['is_conjugated'].index(bond.GetIsConjugated()))
-        e = e[:num_edge_features]
 
         edge_indices += [[i, j], [j, i]]
         edge_attrs += [e, e]
 
     edge_index = torch.tensor(edge_indices)
-    edge_index = edge_index.t().to(torch.long).view(2, -1)  # (2, |E|)
-    edge_attr = torch.tensor(edge_attrs, dtype=torch.float).view(-1, num_edge_features)  # (|E|, len(e))
+    edge_index = edge_index.t().to(torch.long).view(2, -1)
+    edge_attr = torch.tensor(edge_attrs, dtype=torch.float).view(-1, 3)
 
     if edge_index.numel() > 0:  # Sort indices.
         perm = (edge_index[0] * x.size(0) + edge_index[1]).argsort()
